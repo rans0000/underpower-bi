11704f539c26845acf3fc423896c23a965ea32f3
rans0000 <rans0000@gmail.com>
2025-03-11 22:08:30 +0530

    cleanup the starter template

---
+++ src/components/footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="footer-wrapper row-start-3 flex gap-[24px] flex-wrap items-center justify-center border-t-2 border-gray-200 py-2">
+      &copy; underpower-bi
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header className="flex gap-4 align-middle p-4 border-b-2 border-gray-200">
+      <h1 className="italic font-bold">Underpowered BI</h1>
+    </header>
+  );
+}
+
+export default Header;
