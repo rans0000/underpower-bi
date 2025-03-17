d2596efdaca4170046d3fa40bac2597c6a6fbb2b
rans0000 <rans0000@gmail.com>
2025-03-17 13:32:26 +0530

    separate upload form and table view into individual client components

--- src/components/file-uploader.tsx
+++ src/components/file-uploader.tsx
@@ -1,39 +1,27 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { uploadFile } from "@/lib/actions";
-import { DataContext } from "@/lib/data-provider";
-import { TCurrentTab, TUploadFormState } from "@/lib/types";
-import { Dispatch, useActionState, useContext, useEffect } from "react";
-
-const initialValue: TUploadFormState = { file: null, data: null };
+import { TCurrentTab } from "@/lib/types";
+import { Dispatch } from "react";
+import DisplayOriginalData from "./display-original-data";
+import { Button } from "./ui/button";
+import UploadForm from "./upload-form";
 
 function FileUploader({ changeTab }: { changeTab: Dispatch<TCurrentTab> }) {
-  const [state, formAction, isPending] = useActionState(
-    uploadFile,
-    initialValue
-  );
-  const { dispatch } = useContext(DataContext);
-
-  useEffect(() => {
-    dispatch({ type: "original", payload: state.data });
-    if (state.data) {
-      changeTab("transform-data");
-    }
-  }, [state.status]);
-
   return (
-    <div className="p-3">
-      <form className="flex flex-col gap-2 w-3/12" action={formAction}>
-        <Label htmlFor="file">Upload CSV</Label>
-        <Input id="file" name="file" type="file" />
-        <Button type="submit" className="cursor-pointer" disabled={isPending}>
-          Upload
-        </Button>
-        {state.status && <p>{state.status}</p>}
-      </form>
+    <div className="p-3 flex gap-12">
+      <section className="left-wrapper flex-3/12">
+        <UploadForm />
+      </section>
+      <aside className="flex-9/12">
+        <DisplayOriginalData />
+      </aside>
+      <div>
+        <Button
+          onClick={() => {
+            changeTab("transform-data");
+          }}
+        ></Button>
+      </div>
     </div>
   );
 }
---
+++ src/components/upload-form.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { uploadFile } from "@/lib/actions";
+import { DataContext } from "@/lib/data-provider";
+import { useActionState, useContext, useEffect } from "react";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { TUploadFormState } from "@/lib/types";
+
+const initialValue: TUploadFormState = { file: null, data: null };
+
+function UploadForm() {
+  const [state, formAction, isPending] = useActionState(
+    uploadFile,
+    initialValue
+  );
+  const { dispatch } = useContext(DataContext);
+
+  useEffect(() => {
+    dispatch({ type: "original", payload: state.data });
+    if (state.data) {
+    }
+  }, [state.status]);
+
+  return (
+    <form className="flex flex-col gap-2" action={formAction}>
+      <Label htmlFor="file">Upload CSV</Label>
+      <Input id="file" name="file" type="file" />
+      <Button type="submit" className="cursor-pointer" disabled={isPending}>
+        Upload
+      </Button>
+      {state.status && <p>{state.status}</p>}
+    </form>
+  );
+}
+
+export default UploadForm;
