f4cec731d7155cfcdfde9fcead059ef61d442ab9
rans0000 <rans0000@gmail.com>
2025-03-23 17:16:08 +0530

    make the basic node graph interactable

--- src/components/transformed-data-graph.tsx
+++ src/components/transformed-data-graph.tsx
@@ -1,18 +1,48 @@
-import React from "react";
-import { ReactFlow } from "@xyflow/react";
+import {
+  addEdge,
+  Background,
+  Controls,
+  Edge,
+  Node,
+  OnConnect,
+  ReactFlow,
+  useEdgesState,
+  useNodesState,
+} from "@xyflow/react";
+import { useCallback } from "react";
 
 import "@xyflow/react/dist/style.css";
 
-const initialNodes = [
-  { id: "1", position: { x: 0, y: 0 }, data: { label: "1" } },
+const initialNodes: Node[] = [
+  { id: "1", position: { x: 0, y: 0 }, data: { label: "1" }, type: "input" },
   { id: "2", position: { x: 0, y: 100 }, data: { label: "2" } },
 ];
-const initialEdges = [{ id: "e1-2", source: "1", target: "2" }];
+const initialEdges: Edge[] = [{ id: "e1-2", source: "1", target: "2" }];
 
 function TransformedDataNodeGraph() {
+  const [nodes, setNodes, onNodeChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgeChange] = useEdgesState(initialEdges);
+  console.log(setNodes);
+
+  const onConnect: OnConnect = useCallback(
+    (params) => {
+      setEdges((edg) => addEdge(params, edg));
+    },
+    [setEdges]
+  );
+
   return (
     <section className="transformed-data-node-graph w-full h-full">
-      <ReactFlow nodes={initialNodes} edges={initialEdges} />
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodeChange}
+        onEdgesChange={onEdgeChange}
+        onConnect={onConnect}
+      >
+        <Controls />
+        <Background gap={12} size={1} />
+      </ReactFlow>
     </section>
   );
 }
